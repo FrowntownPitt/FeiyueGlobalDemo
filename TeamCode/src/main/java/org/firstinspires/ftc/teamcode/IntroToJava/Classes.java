16dda7d16048ccfe5319f6596815f9971aa05ab8
= <=>
2017-06-14 10:49:45 -0400

    Updated IntroToJava.Classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntroToJava/Classes.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntroToJava/Classes.java
@@ -6,8 +6,11 @@ package org.firstinspires.ftc.teamcode.IntroToJava;
 // This line (followed by the curly braces) defines a "block" of code.  In this case, this block is
 // a "class", or a definition of what the program can do.
 public class Classes {
-    // the above line 
+    // the above line has 3 keywords:
+    // "public" makes this class visible and usable by any other class.
+    // "class" tells Java that this block is a class.
+    // "Classes" is the name of this class.  Class names MUST be the same name as the file!
 
     // This is the "class body".  This area contains all of the code that runs under this class.
-    //
+    // Methods and class members will go here.
 }
